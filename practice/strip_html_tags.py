0f5773176b420bbb9901c7a30b0fc32d3ce5f61b
Ruaraidh <ruaraidh.leary@clyde.space>
2019-07-15 15:00:58 +0100

    reading freezing level and temp

--- practice/strip_html_tags.py
+++ practice/strip_html_tags.py
@@ -25,7 +25,7 @@ def strip_tags(value):
 
 web_address = 'http://www.mwis.org.uk/scottish-forecast/WH/'
 
-decoded_line = ''
+#decoded_line = ''
 
 try:
     response = urlopen(web_address)
@@ -36,11 +36,17 @@ for line in response:
     line = line.decode('utf-8')  # encoding while file in utf8
     if 'Freezing Level' in line:
         freezing_level_raw = next(response)
-        decoded_line = freezing_level_raw.decode('utf-8')
+        decoded_line_freezing = freezing_level_raw.decode('utf-8')
+    if 'How Cold' in line:
+        temp_at_900_raw = next(response)
+        decoded_line_temp = temp_at_900_raw.decode('utf-8')
 
 response.close()
 
 tag_re = re.compile(r'(<!--.*?-->|<[^>]*>)')
-stripped_html_line = tag_re.sub('', decoded_line)
-stripped_html_line = stripped_html_line.strip()
-print(stripped_html_line)
+freezing_level = tag_re.sub('', decoded_line_freezing)
+freezing_level = freezing_level.strip()
+temp_at_900 = tag_re.sub('', decoded_line_temp)
+temp_at_900 = temp_at_900.strip()
+print(freezing_level)
+print(temp_at_900)
