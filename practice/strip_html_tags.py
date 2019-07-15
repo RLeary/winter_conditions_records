30877839d9cd86454e1baa5e8e9768ea87b7dd3d
Ruaraidh <ruaraidh.leary@clyde.space>
2019-07-15 13:59:34 +0100

    more trying to parse html

--- practice/strip_html_tags.py
+++ practice/strip_html_tags.py
@@ -3,7 +3,25 @@
 # get freezing level line from mwis and print
 
 from urllib.request import urlopen
+from django.utils.html import strip_tags
 import re
+
+"""
+# strip_tags From django/utils/html.py
+def strip_tags(value):
+    # Return the given HTML with all tags stripped
+    # Note: in typical case this loop executes _strip_once once. Loop condition
+    # is redundant, but helps to reduce number of executions of _strip_once.
+    value = str(value)
+    while '<' in value and '>' in value:
+        new_value = _strip_once(value)
+        if len(new_value) >= len(value):
+            # _strip_once was not able to detect more tags
+            break
+        value = new_value
+   return value
+"""
+
 
 web_address = 'http://www.mwis.org.uk/scottish-forecast/WH/'
 
@@ -15,11 +33,14 @@ try:
         line = line.decode('utf-8')
         if 'Freezing Level' in line:
             freezing_level_raw = next(response)
-            print(line)
-            print(freezing_level_raw)    
-            line_with_tags = freezing_level_raw      
+            line_with_tags = freezing_level_raw
 except FileNotFoundError:
     print("Address not found")
 response.close()
 
+stripped_html_line = strip_tags(line_with_tags)
+remove_tabs_newlines_line = re.sub(r"[\n\t\s]*", "", stripped_html_line)
+
 print(line_with_tags)
+print(stripped_html_line)
+print(remove_tabs_newlines_line)
