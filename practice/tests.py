6ba34e604f7680f4f64dbd741c8c1b3e32885750
Ruaraidh <ruaraidh.leary@clyde.space>
2019-07-16 16:04:51 +0100

    adding some tests
    adding some tests

---
+++ practice/tests.py
@@ -0,0 +1,7 @@
+import unittest
+
+# HTML reading tests
+Class HTMLReadTests(unittestTestCase):
+    # Test the function to get a list of ints from a string
+    def test_get_int_from_string(self):
+        self.assertEqual
