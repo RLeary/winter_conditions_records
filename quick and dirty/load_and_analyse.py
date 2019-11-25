ce16e0682f7e92cd104e2d0e21ce4fbc4e4791c6
Ruaraidh <ruaraidh.leary@clyde.space>
2019-11-25 14:37:53 +0000

    Load csv files into memory

--- quick and dirty/load_and_analyse.py
+++ quick and dirty/load_and_analyse.py
@@ -9,17 +9,40 @@ csv_files = ['quick and dirty/wh_records.csv',
             'quick and dirty/nw_records.csv'
             ]
 
+## create dict of lists, so all area CSVs can be loaded into lists in a loop?
+#from collections import defaultdict
+#d = defaultdict(list)
+#a = ['1', '2']
+#for i in a:
+#    for j in range(int(i), int(i) + 2):
+#        d[j].append(i)
+#print(d)
+#print(d.items())
+
+class Record:
+    def __init__(self, temp_at_900, freezing_level, date, area):
+        self.freezing_level = freezing_level
+        self.temp_at_900 = temp_at_900
+        self.date = date
+        self.area = area
+
 def read_csv_file(csv_file):
     csv_data = []
     read_file = open(csv_file, 'r')
     csv_reader = csv.reader(read_file)
+    next(csv_reader)
+    next(csv_reader)
     for row in csv_reader:
-        csv_data.append(row)
+        record = Record(row[0], row[1], row[2], row[3])
+        csv_data.append(record)
     read_file.close()
     return csv_data
 
-#csvfile = read_csv_file(csv_files[2])
-#print(csvfile)
+wh_record_list = read_csv_file(csv_files[0])
+eh_record_list = read_csv_file(csv_files[1])
+sh_record_list = read_csv_file(csv_files[2])
+su_record_list = read_csv_file(csv_files[3])
+nw_record_list = read_csv_file(csv_files[4])
 
-for i in range(len(csv_files)):
-xxx = read_csv_file(csv_files[i])
+print(nw_record_list)
+print(nw_record_list[2].date)
