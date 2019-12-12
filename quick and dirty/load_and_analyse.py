65b3eab0c8b723243586cb0b3c8d4b9a98b7bc3c
Ruaraidh <ruaraidh.leary@clyde.space>
2019-12-12 15:36:50 +0000

    loading into Records class

--- quick and dirty/load_and_analyse.py
+++ quick and dirty/load_and_analyse.py
@@ -9,16 +9,6 @@ csv_files = ['quick and dirty/wh_records.csv',
             'quick and dirty/nw_records.csv'
             ]
 
-## create dict of lists, so all area CSVs can be loaded into lists in a loop?
-#from collections import defaultdict
-#d = defaultdict(list)
-#a = ['1', '2']
-#for i in a:
-#    for j in range(int(i), int(i) + 2):
-#        d[j].append(i)
-#print(d)
-#print(d.items())
-
 class Record:
     def __init__(self, temp_at_900, freezing_level, date, area):
         self.freezing_level = freezing_level
@@ -26,6 +16,15 @@ class Record:
         self.date = date
         self.area = area
 
+    # Allow comparison of objects by their variables, fror finding any 
+    # duplicates in lists
+    def __eq__(self, other):
+        if self.__class__ != other.__class__:
+            return False
+        return self.__dict__ == other.__dict__
+
+
+# Return list of objects
 def read_csv_file(csv_file):
     csv_data = []
     read_file = open(csv_file, 'r')
@@ -37,6 +36,26 @@ def read_csv_file(csv_file):
         csv_data.append(record)
     read_file.close()
     return csv_data
+
+# Return list of lists
+def read_csv_file_return_list_of_lists(csv_file):
+    csv_data = []
+    read_file = open(csv_file, 'r')
+    csv_reader = csv.reader(read_file)
+    next(csv_reader)
+    next(csv_reader)
+    for row in csv_reader:
+        csv_data.append(row)
+    read_file.close()
+    return csv_data
+
+# delete duplicate dates in a list
+def del_dup_dates(records_list):
+    pass
+#for i in range(len(records_list)-1):
+#   if records_list[i].date == records_list[i+1].date:
+#       del records_list[i]
+
 
 wh_record_list = read_csv_file(csv_files[0])
 eh_record_list = read_csv_file(csv_files[1])
